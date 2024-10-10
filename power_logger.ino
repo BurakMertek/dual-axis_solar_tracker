bfc61c6b495d499c49a096580122b5719c4a90ab
BurakMertek <bm.mertek@gmail.com>
2024-10-10 19:51:27 +0300

    power logger prepared

---
+++ power_logger.ino
@@ -0,0 +1,9 @@
+/*
+ * Solar Power Logger System
+ * Power monitoring and data logging for solar tracking system
+ * 
+ * Components:
+ * - INA219 current/voltage sensor
+ * - I2C OLED display
+ * - TP4056 battery charger
+ */
